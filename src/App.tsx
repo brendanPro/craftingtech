340ef6fda7bdb560f84eb4d60b82c7e4520f709e
brendiche <brendan.gouin92@gmail.com>
2025-03-02 10:50:00 +0100

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import ChangeMe from './components/changeme'
+import Header from './components/header'
+import CraftingTech from './components/onepager'
+
+function App() {
+
+  return (
+    <>
+      <CraftingTech />
+      {/* <Header />
+      <ChangeMe /> */}
+    </>
+  )
+}
+
+export default App
